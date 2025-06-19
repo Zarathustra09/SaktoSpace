f9eb307cb03bc376e9920b145a023a2cd3c8cc23
Fumihito <moralescarljhon@gmail.com>
2025-06-19 10:41:44 +0800

    Add new images for furniture categories and update references in the code

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,6 +1,7 @@
 class CategoryModel {
   final String title;
-  final String? image, svgSrc;
+  final String? image;
+  final String? svgSrc;
   final List<CategoryModel>? subCategories;
 
   CategoryModel({
@@ -11,49 +12,63 @@ class CategoryModel {
   });
 }
 
+// Categories with images (for main shop screen tiles)
 final List<CategoryModel> demoCategoriesWithImage = [
-  CategoryModel(title: "Woman’s", image: "https://i.imgur.com/5M89G2P.png"),
-  CategoryModel(title: "Man’s", image: "https://i.imgur.com/UM3GdWg.png"),
-  CategoryModel(title: "Kid’s", image: "https://i.imgur.com/Lp0D6k5.png"),
-  CategoryModel(title: "Accessories", image: "https://i.imgur.com/3mSE5sN.png"),
+  CategoryModel(
+    title: "Furniture",
+    image: "assets/images/furniture1.png",
+  ),
+  CategoryModel(
+    title: "Decor",
+    image: "assets/images/decor1.png",
+  ),
+  CategoryModel(
+    title: "Lighting",
+    image: "assets/images/lighting1.png",
+  ),
+  CategoryModel(
+    title: "Outdoor",
+    image: "assets/images/outdoor1.png",
+  ),
 ];
 
+// Categories with optional icons and subcategories
 final List<CategoryModel> demoCategories = [
   CategoryModel(
-    title: "On sale",
-    svgSrc: "assets/icons/Sale.svg",
+    title: "Furniture",
+    svgSrc: "assets/icons/furniture.svg",
     subCategories: [
-      CategoryModel(title: "All Clothing"),
-      CategoryModel(title: "New In"),
-      CategoryModel(title: "Coats & Jackets"),
-      CategoryModel(title: "Dresses"),
-      CategoryModel(title: "Jeans"),
+      CategoryModel(title: "Sofas"),
+      CategoryModel(title: "Beds"),
+      CategoryModel(title: "Dining Tables"),
+      CategoryModel(title: "Chairs"),
     ],
   ),
   CategoryModel(
-    title: "Man’s & Woman’s",
-    svgSrc: "assets/icons/Man&Woman.svg",
+    title: "Decor",
+    svgSrc: "assets/icons/decor.svg",
     subCategories: [
-      CategoryModel(title: "All Clothing"),
-      CategoryModel(title: "New In"),
-      CategoryModel(title: "Coats & Jackets"),
+      CategoryModel(title: "Wall Art"),
+      CategoryModel(title: "Mirrors"),
+      CategoryModel(title: "Vases"),
     ],
   ),
   CategoryModel(
-    title: "Kids",
-    svgSrc: "assets/icons/Child.svg",
+    title: "Lighting",
+    svgSrc: "assets/icons/lighting.svg",
     subCategories: [
-      CategoryModel(title: "All Clothing"),
-      CategoryModel(title: "New In"),
-      CategoryModel(title: "Coats & Jackets"),
+      CategoryModel(title: "Ceiling Lights"),
+      CategoryModel(title: "Lamps"),
+      CategoryModel(title: "LED Strips"),
     ],
   ),
   CategoryModel(
-    title: "Accessories",
-    svgSrc: "assets/icons/Accessories.svg",
+    title: "Outdoor",
+    svgSrc: "assets/icons/outdoor.svg",
     subCategories: [
-      CategoryModel(title: "All Clothing"),
-      CategoryModel(title: "New In"),
+      CategoryModel(title: "Garden Furniture"),
+      CategoryModel(title: "Patio Sets"),
+      CategoryModel(title: "Umbrellas"),
     ],
   ),
 ];
